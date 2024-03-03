82e1ab683e3e747b47c37c623e86e4bb3d471979
fpm.karnienk <dashkarni@gmail.com>
2024-03-03 18:41:32 +0300

    Create asm_laba_3_1.cpp

---
+++ asm_laba_3/asm_laba_3_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+
+int main() {
+
+	int a, res;
+	std::cin >> a;
+
+	_asm {
+
+		mov ecx, 11
+		mov eax, a
+			beg1:
+		imul eax, a
+			loop beg1
+		
+		mov ecx, 7
+		mov ebx, a
+			beg2:
+		imul ebx, a
+			loop beg2
+		add eax, ebx
+		add eax, a
+		mov res, eax
+	}
+
+	std::cout << res << "\n";
+
+	return 0;
+}
