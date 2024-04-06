4f678b35dcf4be044e90bbcdf710b3854c39a2c7
fpm.karnienk <dashkarni@gmail.com>
2024-04-06 17:50:50 +0300

    Create coproc_5_1.cpp

---
+++ coproc_5_1.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <iomanip> 
+
+int main() {
+
+	float k = 1;
+	float res = 0;
+	float one = 1;
+	float minusone = -1;
+	float fract = 0;
+	float sum = 0;
+	float onechg = -1;
+	float twelve = 12;
+	float pi = 0;
+	float trash;
+	_asm{
+	mov ecx, 1500
+
+//	fld minusone
+///	fld onechg
+//	fadd
+//	fstp onechg//-1
+	sumloop:
+		fld k
+		fld k
+		fmul
+		fld one
+		fdiv st(0), st(1)
+		fld fract
+		fadd 
+		fstp fract
+
+		fld minusone
+		fld onechg
+		fmul
+		fstp onechg
+	
+
+		fld onechg
+        fld fract
+		fmul
+		fstp fract//prettier
+
+		fld fract//
+		fld sum
+		fadd 
+		fstp sum
+
+		fstp trash
+
+		fld minusone
+		fld fract
+		fmul
+		fld fract
+		fadd
+		fstp fract
+
+		fld one
+		fld k
+		fadd
+		fstp k
+		
+		loop sumloop
+
+		fld twelve
+		fld sum
+		fmul
+	    fsqrt
+		fstp sum
+
+		fld res
+		fld sum
+		fadd
+		fstp res
+		
+		fld res
+		fldpi
+	    fcomp
+
+		fldpi
+		fstp pi
+	}
+	std::cout << std::fixed;
+	std::cout << std::setprecision(20) << pi << " - real pi" << "\n";
+	std::cout << std::fixed;
+	std::cout << std::setprecision(5) << res << "\n";
+	std::cout << std::setprecision(7) << res << "\n";
+	std::cout << std::setprecision(9) << res << "\n";
+	return 0;
+}
